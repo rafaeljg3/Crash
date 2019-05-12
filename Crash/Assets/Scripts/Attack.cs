502c1d46b774d2f065c3cbe8422a498a8ee1c5cc
rafaeljg3 <rafaelgoncalves92@gmail.com>
2019-05-12 06:31:59 -0300

    Enemy

---
+++ Crash/Assets/Scripts/Attack.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public float forceHorizontal = 15;
+    public float forceVertical = 10;
+    public float timeDestruction = 1;
+
+    private float forceHorizontalDefault;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        forceHorizontalDefault = forceHorizontal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<Enemy>().enabled = false;
+
+            BoxCollider2D[] boxes = other.gameObject.GetComponents<BoxCollider2D>();
+
+            foreach(BoxCollider2D box in boxes)
+            {
+                box.enabled = false;
+            }
+
+            if(other.transform.position.x < transform.position.x)
+            {
+                forceHorizontal *= -1;
+            }
+
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(forceHorizontal, forceVertical), ForceMode2D.Impulse);
+
+            Destroy(other.gameObject, timeDestruction);
+
+            forceHorizontal = forceHorizontalDefault;
+        }
+    }
+}
---
+++ Crash/Assets/Scripts/Enemy.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float speed;
+
+    private bool facingRight = false;
+
+    public float jumpForce = 1000;
+    private bool isGround = false;
+    private Transform groundCheck;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();       
+        groundCheck = gameObject.transform.Find("EnemyGroundCheck");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if(!isGround)
+        {
+            speed *= -1;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+
+        if(speed > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if(speed < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+ 
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            BoxCollider2D[] boxes = other.gameObject.GetComponents<BoxCollider2D>();
+
+            foreach(BoxCollider2D box in boxes)
+            {
+                box.enabled = false;
+            }
+
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+
+            speed = 0;
+
+            transform.Rotate(new Vector3(0, 0, -180));
+            
+            Destroy(gameObject);
+        }
+    }
+}
