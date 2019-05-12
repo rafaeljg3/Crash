78d9c3edde957b06cffef947cbfc1f43ffa60700
rafaeljg3 <rafaelgoncalves92@gmail.com>
2019-05-12 04:11:44 -0300

    Attack

---
+++ Crash/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Animator anim;
+
+    public float rangeAttack;
+    private float nextAttack;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && Time.time > nextAttack)
+        {
+            Attacking();
+        }
+    }
+
+    void Attacking()
+    {
+        anim.SetTrigger("Attack");
+        nextAttack = Time.time + rangeAttack;
+    }
+}
